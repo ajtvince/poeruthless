4fae40a2bbb5b04510165a9b4e883b18aaffc0ab
ajtvince <41200248+ajtvince@users.noreply.github.com>
2023-03-31 16:46:29 -0400

    intiial pages

---
+++ LeagueContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Stylesheets/LeagueContent.css';
+//show league content that is in ruthless and info
+function LeagueContent() {
+  return (
+    <div className='pageContainer'>
+      <p>LeagueContent</p>
+    </div>
+  );
+}
+
+export default LeagueContent;
