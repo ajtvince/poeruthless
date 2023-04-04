e93d9c6eb38975415f677925d207673b16c8fd78
AC42277 <andrew_vince@goodyear.com>
2023-04-04 10:22:54 -0400

    added glow notifier when gem card changes

--- poe-ruthless-tools/src/routes/GemGuide_good.js
+++ poe-ruthless-tools/src/routes/GemGuide_good.js
@@ -324,6 +324,44 @@ export default function GemGuide() {
     return arr[minIndex];
   }
 
+  function GemIcon({ gem, removeGemFromList }) {
+    const [flash, setFlash] = useState(false);
+
+    useEffect(() => {
+      // Set flash to true when gem.name changes
+      setFlash(true);
+
+      // Set flash back to false after the animation ends
+      const timeoutId = setTimeout(() => {
+        setFlash(false);
+      }, 800);
+  
+      // Clear the timeout if the component unmounts before the animation ends
+      return () => clearTimeout(timeoutId);
+    }, [gem.name]);
+
+    const style = flash ? { transform: 'scale(1.05)' } : {}
+  
+    return (
+      <div key={gem.class + '' + gem.name} className='gemIconContainer' style={style}>
+        <div className='gemIconPic'>
+          <img src='/media/Absolution_skill_icon.png' alt='-------'/>
+        </div>
+        <div className='gemIconName'>{gem.name}</div>
+        <div className='gemIconContainerL'>
+          <div className='gemIconClassLabel'>Class: </div>
+          <div className='gemIconQuestLabel'>Quest: </div>
+        </div>
+        <div className='gemIconContainerR'>
+          <div className='gemIconClass'>{gem.class}</div>
+          <div className='gemIconQuest'>Act {gem.act}: {gem.quest}</div>
+        </div>
+        <div className='gemIconDelete' onClick={() => removeGemFromList(gem)}>X</div>
+        <a href={'https://www.poewiki.net/wiki/' + gem.name} className='gemIconWiki'>wiki</a>
+      </div>
+    );
+  }
+
   //fill initial selectable gems list, and update based on search query
   useEffect(() => {
     
@@ -384,72 +422,4 @@ export default function GemGuide() {
       </div>
     </div>
   );
-}
-
-class GemIcon extends React.Component<{isFlash: boolean}> {
-  constructor(props) {
-    super(props);
-    this.state = {isFlash: false, styling: {transform: 'scale(1.05'} };
-    console.log(this.state);
-  }
-
-  handleStatusChange(status) {
-
-    console.log('status change');
-    this.setState({
-      isFlash: true
-    });
-
-    console.log(this.state);
-
-    // Set flash back to false after the animation ends
-    setTimeout(() => {
-      this.setState({
-        isFlash: false,
-        styling: {},
-      });
-    }, 800);
-
-    // Clear the timeout if the component unmounts before the animation ends
-    //return () => clearTimeout(timeoutId);
-  }
-
-  componentDidMount() {
-    console.log('test mounted');
-    this.setState({isFlash: true, styling: {transform: 'scale(1.05)', boxShadow: 'yellow 0px 0px 20px'}});
-    setTimeout(() => {
-      this.setState({
-        isFlash: false,
-        styling: {},
-      });
-    }, 800);
-
-  }
-  componentDidUpdate(prevProps, prevState) {
-
-    console.log('test updated');
-
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <div className='gemIconContainer' style={this.state.styling}>
-        <div className='gemIconPic'>
-          <img src='/media/Absolution_skill_icon.png' alt='-------'/>
-        </div>
-        <div className='gemIconName'>{this.props.gem.name}</div>
-        <div className='gemIconContainerL'>
-          <div className='gemIconClassLabel'>Class: </div>
-          <div className='gemIconQuestLabel'>Quest: </div>
-        </div>
-        <div className='gemIconContainerR'>
-          <div className='gemIconClass'>{this.props.gem.class}</div>
-          <div className='gemIconQuest'>Act {this.props.gem.act}: {this.props.gem.quest}</div>
-        </div>
-        <div className='gemIconDelete' onClick={() => this.props.removeGemFromList(this.props.gem)}>X</div>
-        <a href={'https://www.poewiki.net/wiki/' + this.props.gem.name} className='gemIconWiki'>wiki</a>
-      </div>
-    );
-  }
 }
